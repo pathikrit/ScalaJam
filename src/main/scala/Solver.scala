6a802ee6bc6ccd12a873153e46c5afdd3a9d240b
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2017-01-11 11:06:56 -0500

    use auto-closed iterators

--- src/main/scala/Solver.scala
+++ src/main/scala/Solver.scala
@@ -1,14 +1,16 @@
 import better.files._
 abstract class Solver(inputFile: String) extends App {
-  def solve(): Any
-  val in = Scanner(getClass.getResourceAsStream(inputFile))(Scanner.Config.default)
-  val out = File(inputFile.replace(".in", ".out")).newOutputStream.printer()
   println(s"Solving: $inputFile")
-  for (i <- 1 to in.next[Int]) {
-    val output = s"Case #$i: ${solve()}"
+
+  def solve(): Any
+
+  for {
+    in <- Scanner(getClass.getResourceAsStream(inputFile))(Scanner.Config.default).autoClosed
+    out <- File(inputFile.replace(".in", ".out")).newOutputStream.printer().autoClosed
+    i <- 1 to in.next[Int]
+    output = s"Case #$i: ${solve()}"
+  } {    
     println(output)
     out.println(output)
   }
-  in.close()
-  out.close()
 }
